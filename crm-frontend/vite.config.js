7b7a6800b51b642ab6780f02cdcec05fd487a58c
Dilane <dylan@example.com>
2026-01-05 14:15:20 +0100

    Mise à jour de la configuration de base dans vite.config.js pour les chemins relatifs sur GitHub Pages.

--- crm-frontend/vite.config.js
+++ crm-frontend/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./", // Pour les chemins relatifs sur GitHub Pages
+  base: "/Dilane17/crm/", // Pour les chemins relatifs sur GitHub Pages
 });

4620add7a2b83acbaee2f71159e143a88e181154
Dilane <dylan@example.com>
2026-01-05 15:31:30 +0100

    Changement de HashRouter à BrowserRouter avec ajout de la propriété basename dans main.jsx

--- crm-frontend/src/main.jsx
+++ crm-frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
+    <Router basename="/crm">
       <AuthProvider>
         <App />
       </AuthProvider>
